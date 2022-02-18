8ec1d3ec43ab01378e9e0e55c57dfa23644549ff
Abanoub Ghaly <abanoub.emil@almtar.com>
2022-02-18 16:04:13 +0200

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Abanoub Emil on 18/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
